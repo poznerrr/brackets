0fa22682439f7273f417c3990c57671fddb886e4
pozner <poznerrr@gmail.com>
2021-03-14 19:07:10 +0300

    feat: do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length%2!=0){
+    return false;
+  }
+  let arr=str.split('');
+  let flag=1;
+  while(arr.length>0 && flag==1){
+    flag=0;
+   for (let i=0; i<arr.length-1; i++){
+     let pre=arr[i];
+     let post;
+      for(let miniarr of bracketsConfig){
+        if (pre==miniarr[0])
+        {
+          post=miniarr[1];
+        }
+      }
+    if (arr[i+1]==post){
+      arr.splice(i,2);
+      flag=1;
+      break;
+    }
+   }
+   if (flag==0){
+     return false;
+    }
+  }
+  return true;
 }
